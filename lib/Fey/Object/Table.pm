55d995b467d263dbe5592e6205c9b42e1739ae7d
autarch <devnull@localhost>
2008-03-14 23:49:23 +0000

    Don't try to return pk if we don't have all the pk attributes set

--- lib/Fey/Object/Table.pm
+++ lib/Fey/Object/Table.pm
@@ -373,7 +373,18 @@ sub _pk_vals
 {
     my $self = shift;
 
-    return map { $self->$_() } map { $_->name() } $self->Table()->primary_key();
+    my @pk;
+
+    for my $col_name ( map { $_->name() } $self->Table()->primary_key() )
+    {
+        my $pred = 'has_' . $col_name;
+
+        return unless $self->$pred();
+
+        push @pk, $self->$col_name();
+    }
+
+    return @pk;
 }
 
 sub _MakeSelectByPKSQL
@@ -584,7 +595,9 @@ If there is no source for the given SQL, it will die.
 =head2 $object->_pk_vals()
 
 This method returns an array of primary key values for the object's
-row.
+row. This may return an empty array if the primary key for the object
+has not yet been determined. This can happen if you try to call this
+on an object before its attributes have been fetched from the dbms.
 
 =head1 AUTHOR
 
