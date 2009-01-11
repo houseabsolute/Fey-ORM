3dc5c2bb9b3536d39a3fada299724762b6babeb3
Dave Rolsky <autarch@urth.org>
2009-01-11 21:22:30 +0800

    Add docs

--- lib/Fey/Meta/Attribute/Inflated.pm
+++ lib/Fey/Meta/Attribute/Inflated.pm
@@ -32,3 +32,58 @@ no Moose;
 __PACKAGE__->meta()->make_immutable( inline_constructor => 0 );
 
 1;
+
+__END__
+
+=head1 NAME
+
+Fey::Meta::Attribute::Inflated - an attribute metaclass for inflated attributes
+
+=head1 SYNOPSIS
+
+  package MyApp::Song;
+
+  has_table( $schema->table('Song') );
+
+  for my $attr ( grep { $_->can('raw_attribute') } $self->meta()->get_all_attributes )
+  {
+      ...
+  }
+
+=head1 DESCRIPTION
+
+This attribute metaclass is used when L<Fey::ORM::Table> creates
+attributes based on an inflator transform.
+
+=head1 METHODS
+
+This class adds a two methods to those provided by
+C<Moose::Meta::Attribute>:
+
+=head2 $attr->raw_attribute()
+
+Returns the attribute for the raw version of this data. This is the
+original attribute created for the column, which was renamed when the
+inflator was declared.
+
+=head2 $attr->column()
+
+Returns the C<Fey::Column> object associated with the raw attribute.
+
+=head1 AUTHOR
+
+Dave Rolsky, <autarch@urth.org>
+
+=head1 BUGS
+
+See L<Fey::ORM> for details.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006-2009 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. The full text of the license
+can be found in the LICENSE file included with this module.
+
+=cut
