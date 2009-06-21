e5f7640560409fbdf4558f82289578f9c9c095b4
Dave Rolsky <autarch@urth.org>
2009-06-21 15:19:42 -0500

    Fixes to work with next Moose

--- lib/Fey/Meta/Method/Constructor.pm
+++ lib/Fey/Meta/Method/Constructor.pm
@@ -76,7 +76,7 @@ sub _initialize_body {
         defined $_ ? $_->_compiled_type_constraint : undef;
     } @type_constraints;
 
-    my $code = $self->_compile_code(
+    my ( $code, $e ) = $self->_compile_code(
         code => $source,
         environment => {
             '$meta'      => \$self,
@@ -85,12 +85,15 @@ sub _initialize_body {
             '@type_constraints' => \@type_constraints,
             '@type_constraint_bodies' => \@type_constraint_bodies,
         },
-    ) or $self->throw_error("Could not eval the constructor :\n\n$source\n\nbecause :\n\n$@", error => $@, data => $source );
+    );
+
+    $self->throw_error("Could not eval the constructor :\n\n$source\n\nbecause :\n\n$e", error => $e, data => $source )
+        if $e;
 
     $self->{'body'} = $code;
 }
 
-sub _expected_constructor_class {
+sub _expected_method_class {
     return 'Fey::Object::Table';
 }
 
