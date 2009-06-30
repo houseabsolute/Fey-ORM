7c742c061bb4e348d6f722bf75aff6f8cde1b779
Dave Rolsky <autarch@urth.org>
2009-06-30 14:52:38 -0500

    Revert change to set constructor class in _new

--- lib/Fey/Meta/Class/Table.pm
+++ lib/Fey/Meta/Class/Table.pm
@@ -170,17 +170,6 @@ sub _ClassForTable
     return;
 }
 
-sub _new {
-    my $class = shift;
-
-    my $self = $class->SUPER::_new(@_);
-
-    $self->{constructor_class} = 'Fey::Meta::Method::Constructor';
-
-    return $self;
-}
-
-
 sub _search_cache
 {
     my $self = shift;
@@ -501,6 +490,14 @@ sub _build__count_sql
 
     return $select;
 }
+
+sub make_immutable {
+    shift->SUPER::make_immutable
+        ( @_,
+          constructor_class => 'Fey::Meta::Method::Constructor',
+        );
+}
+
 
 no Moose;
 
